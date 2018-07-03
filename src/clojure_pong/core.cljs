875c5278addc1190853c9ea334c55bd3ca38dc22
Aurélien Bottazini <aurelien@bottazini.com>
2018-07-03 13:51:10 +0200

    Refactor scores to app-state atom

--- src/clojure_pong/core.cljs
+++ src/clojure_pong/core.cljs
@@ -3,6 +3,11 @@
 
 (enable-console-print!)
 
+;; define your app data so that it doesn't get over-written on reload
+(defonce app-state (atom {:text "Hello world!"
+                          :playerScore 0
+                          :computerScore 0 }))
+
 (defn getCanvasDimensions [canvas]
   (let [width (.-offsetWidth canvas)
         height (.-offsetHeight canvas)]
@@ -15,18 +20,17 @@
   (.getContext (getCanvas) "2d"))
 
 (defn drawGameShell [context]
-  (let [width (:width (getCanvasDimensions (getCanvas)))
-        height (:height (getCanvasDimensions (getCanvas)))]
+  (let [canvasDimensions (getCanvasDimensions (getCanvas))
+        width (:width canvasDimensions)
+        height (:height canvasDimensions)]
     (doto context
       (aset "fillStyle" "black")
       (.fillRect 0 0 width height)
       (aset "fillStyle" "white")
-      (aset "font" "40px monospace")
-      (.fillText 0 (/ (* width 3) 8) 50)
-      (.fillText 0 (/ (* width 5) 8) 50))))
+      (aset "font" "24px monospace")
+      (.fillText (:playerScore @app-state) (* width (/ 3 8)) 50)
+      (.fillText (:computerScore @app-state) (* width (/ 5 8)) 50))))
 
-;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:text "Hello world!"}))
 (drawGameShell (getContext))
 
 (defn on-js-reload []
