e3e499e1fe59c0f0a93aa87c1fc7588987bcd6a5
Aurélien Bottazini <aurelien@bottazini.com>
2018-07-12 12:31:39 +0200

    Add reset button bind to r

--- src/clojure_pong/core.cljs
+++ src/clojure_pong/core.cljs
@@ -34,15 +34,6 @@
                                 :height 40
                                 }))
 
-(cevent/listen (getCanvas) "keydown" #(let [key-pressed (.-key %1)
-                                            canvasDimensions (getCanvasDimensions (getCanvas))
-                                            height (:height canvasDimensions)
-                                            atFarEdge (> (:y @player-paddle) (- height 40))
-                                            atLowerEdge (< (:y @player-paddle) 0)]
-                                        (cond
-                                          (= key-pressed "ArrowUp") (if-not atLowerEdge (swap! player-paddle update-in [:y] - @playerVelocity))
-                                          (= key-pressed "ArrowDown") (if-not atFarEdge (swap! player-paddle update-in [:y] + @playerVelocity))
-                                          :else (js/console.log "Key not used by game" key-pressed))))
 
 (defn drawGameShell [context]
   (let [canvasDimensions (getCanvasDimensions (getCanvas))
@@ -66,6 +57,7 @@
       (.fillRect (:x @player-paddle) (:y @player-paddle) (:width @player-paddle) (:height @player-paddle))
       (.fillRect (:x @computer-paddle) (:y @computer-paddle) (:width @computer-paddle) (:height @computer-paddle)))))
 
+
 (defn ball-starting-position []
   (let [canvasDimensions (getCanvasDimensions (getCanvas))
         width (:width canvasDimensions)
@@ -74,7 +66,6 @@
         startingX (- (/ width 2) (/ 10 2))]
     { :x startingX :y startingY})
   )
-
 
 (defn gen-velocity [] (let [
                         min-angle -30
@@ -94,21 +85,21 @@
 
 (defonce ball-position (atom { :x (:x (ball-starting-position)), :y (:y (ball-starting-position)), :width 10, :height 10 }))
 
+(defn reset-ball []
+  (swap! ball-position assoc :x (:x (ball-starting-position)) :y (:y (ball-starting-position)))
+  (reset! velocity (gen-velocity)))
+
 (defn intersect [a b]
   (and (> (+ (:y a) (:height a)) (:y b))
        (< (:y a) (+ (:y b) (:height b)))
        (> (+ (:x a) (:width a)) (:x b))
        (< (:x a) (+ (:x b) (:width b)))))
 
-(defn reset-ball []
-  (swap! ball-position assoc :x (:x (ball-starting-position)) :y (:y (ball-starting-position)))
-  (reset! velocity (gen-velocity)))
-
 (add-watch ball-position :velocity (fn [_k _r _o n]
                                        (cond
                                          (intersect n @player-paddle) (do
                                                                         (swap! velocity update-in [:x-velocity] unchecked-negate)
-                                                                        (swap! velocity update-in [:x-velocity] #(* 1.1 %1))
+                                                                        (swap! velocity update-in [:x-velocity] #(* 1.3 %1))
                                                                         )
                                          (intersect n @computer-paddle) (swap! velocity update-in [:x-velocity] unchecked-negate)
                                          (< (:x n) 0) (do (swap! app-state update-in [:computerScore] inc) (reset-ball))
@@ -131,8 +122,8 @@
   (let [ball-y (+ (:y @ball-position) (/ (:height @ball-position) 2))
         computer-y (+ (:y @computer-paddle) (/ (:height @computer-paddle) 2))]
     (cond
-      (< ball-y computer-y) (swap! computer-paddle update-in [:y] dec)
-      (> ball-y computer-y) (swap! computer-paddle update-in [:y] inc)
+      (< ball-y computer-y) (swap! computer-paddle update-in [:y] #(- %1 0.15))
+      (> ball-y computer-y) (swap! computer-paddle update-in [:y] #(+ 0.15 %1))
       :else nil)))
 
 (defn updateBoard []
@@ -147,6 +138,19 @@
                      (drawPaddles (getContext))
                      (drawBall (getContext))
                      (updateBoard))) 25)
+
+
+(cevent/listen (getCanvas) "keydown" #(let [key-pressed (.-key %1)
+                                            canvasDimensions (getCanvasDimensions (getCanvas))
+                                            height (:height canvasDimensions)
+                                            atFarEdge (> (:y @player-paddle) (- height 40))
+                                            atLowerEdge (< (:y @player-paddle) 0)]
+                                        (cond
+                                          (= key-pressed "ArrowUp") (if-not atLowerEdge (swap! player-paddle update-in [:y] - @playerVelocity))
+                                          (= key-pressed "ArrowDown") (if-not atFarEdge (swap! player-paddle update-in [:y] + @playerVelocity))
+                                          (= key-pressed "r") (reset-ball)
+                                          :else (js/console.log "Key not used by game" key-pressed))))
+
 
 (defonce game-loop (gameLoop))
 
